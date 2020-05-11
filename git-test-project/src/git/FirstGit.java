8c299c8ffc4da1cffd9f53429a1850e2e27ec76c
xiaolong <980286246@qq.com>
2020-05-11 17:28:00 +0800

    添加代码再次提交到远程仓库

--- git-test-project/src/git/FirstGit.java
+++ git-test-project/src/git/FirstGit.java
@@ -11,8 +11,11 @@ public class FirstGit {
         System.out.println("hello,git!!!!!");
         System.out.println("hello,git!!!!!");
         test();
+        //提价到远程仓库后在更新原始代码
     }
     public static void test(){
+
         System.out.println("新增方法");
+        System.out.println("克隆完成后再次提交到远程仓库");
     }
 }
